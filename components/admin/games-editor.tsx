40750abc5af71ce5606f82e5b8fb7a5c5057327f
Ravildg <12zapisok.nn@gmail.com>
2025-04-03 21:26:50 +0300

    Update games-editor.tsx

--- components/admin/games-editor.tsx
+++ components/admin/games-editor.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 
+// Структура для игры
 interface Game {
   title: string;
   description: string;
@@ -9,6 +10,7 @@ interface Game {
   link: string;
 }
 
+// Список доступных изображений для выбора
 const availableImages = [
   "/uploads/bt1.jpg",
   "/uploads/comanda.jpg",
@@ -22,10 +24,12 @@ const availableImages = [
   "/uploads/logo.png",
 ];
 
+// Начальные данные для игр
 const initialGamesData: Game[] = [
   {
     title: "Коллекционер Игр",
-    description: "Лондон, туман, ритуальные убийства и исчезнувшие артефакты. Вас ждёт расследование мистического дела в плену у древней игры.",
+    description:
+      "Лондон, туман, ритуальные убийства и исчезнувшие артефакты. Вас ждёт расследование мистического дела в плену у древней игры.",
     players: "6-12 человек",
     tags: "Мистика, Детектив",
     image: "/uploads/ki1.jpg",
@@ -33,7 +37,8 @@ const initialGamesData: Game[] = [
   },
   {
     title: "Берму́дский Треугольник",
-    description: "Остров, на котором всё не так. Странные события, весёлое безумие и комедия на грани фантастики.",
+    description:
+      "Остров, на котором всё не так. Странные события, весёлое безумие и комедия на грани фантастики.",
     players: "8-15 человек",
     tags: "Комедия, Фантастика",
     image: "/uploads/ki2.jpg",
@@ -41,7 +46,8 @@ const initialGamesData: Game[] = [
   },
   {
     title: "Кланы Нью-Йорка",
-    description: "Сигары, виски и рулетка. Гангстерские интриги в атмосфере подпольного казино тридцатых.",
+    description:
+      "Сигары, виски и рулетка. Гангстерские интриги в атмосфере подпольного казино тридцатых.",
     players: "10-20 человек",
     tags: "Гангстеры, Казино",
     image: "/uploads/ki3.jpg",
@@ -49,7 +55,8 @@ const initialGamesData: Game[] = [
   },
   {
     title: "Петля Времени",
-    description: "Механизмы, алхимия и свет во тьме. Вернитесь назад в будущее и раскройте тайну волшебной хижины.",
+    description:
+      "Механизмы, алхимия и свет во тьме. Вернитесь назад в будущее и раскройте тайну волшебной хижины.",
     players: "6-12 человек",
     tags: "Стимпанк, Головоломки",
     image: "/uploads/ki4.jpg",
@@ -57,7 +64,8 @@ const initialGamesData: Game[] = [
   },
   {
     title: "Яхта",
-    description: "Послевоенный рейс — к новой надежде. Яхта, документы, драгоценности — и каждый пассажир не тот, за кого себя выдает.",
+    description:
+      "Послевоенный рейс — к новой надежде. Яхта, документы, драгоценности — и каждый пассажир не тот, за кого себя выдает.",
     players: "8-16 человек",
     tags: "Детектив, Интриги",
     image: "/uploads/ki5.jpg",
@@ -67,119 +75,128 @@ const initialGamesData: Game[] = [
 ];
 
 export default function GamesEditor() {
-  const [gamesData, setGamesData] = useState<Game[]>([]);
-  const [newGame, setNewGame] = useState<Game>({
-    title: "",
-    description: "",
-    players: "",
-    tags: "",
-    image: "",
-    link: "",
-  });
+  const [gamesData, setGamesData] = useState<Game[]>(initialGamesData);
 
+  // Сохраняем игры в localStorage
   useEffect(() => {
     const savedGames = localStorage.getItem("savedGames");
     if (savedGames) {
       setGamesData(JSON.parse(savedGames));
-    } else {
-      setGamesData(initialGamesData);
     }
   }, []);
 
   const handleInputChange = (
     e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
-    field: keyof Game
+    field: keyof Game,
+    index: number
   ) => {
     const { value } = e.target;
-    setNewGame((prevGame) => ({
-      ...prevGame,
+    const updatedGames = [...gamesData];
+    updatedGames[index] = {
+      ...updatedGames[index],
       [field]: value,
-    }));
+    };
+    setGamesData(updatedGames);
+    localStorage.setItem("savedGames", JSON.stringify(updatedGames));
   };
 
-  const handleImageChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+  const handleImageChange = (e: React.ChangeEvent<HTMLSelectElement>, index: number) => {
     const selectedImage = e.target.value;
-    setNewGame((prevGame) => ({
-      ...prevGame,
+    const updatedGames = [...gamesData];
+    updatedGames[index] = {
+      ...updatedGames[index],
       image: selectedImage,
-    }));
+    };
+    setGamesData(updatedGames);
+    localStorage.setItem("savedGames", JSON.stringify(updatedGames));
   };
 
-  const handleSave = () => {
-    const updatedGamesData = [...gamesData, newGame];
-    setGamesData(updatedGamesData);
-    localStorage.setItem("savedGames", JSON.stringify(updatedGamesData));
+  const handleSave = (index: number) => {
+    const updatedGames = [...gamesData];
+    updatedGames[index] = {
+      ...updatedGames[index],
+    };
+    setGamesData(updatedGames);
+    localStorage.setItem("savedGames", JSON.stringify(updatedGames));
   };
 
-  const handleGameSave = (gameIndex: number) => {
-    const updatedGamesData = [...gamesData];
-    updatedGamesData[gameIndex] = newGame;
-    setGamesData(updatedGamesData);
-    localStorage.setItem("savedGames", JSON.stringify(updatedGamesData));
-  };
-
-  const handleRemoveGame = (gameIndex: number) => {
-    const updatedGamesData = gamesData.filter((_, index) => index !== gameIndex);
-    setGamesData(updatedGamesData);
-    localStorage.setItem("savedGames", JSON.stringify(updatedGamesData));
+  const handleRemoveGame = (index: number) => {
+    const updatedGames = gamesData.filter((_, i) => i !== index);
+    setGamesData(updatedGames);
+    localStorage.setItem("savedGames", JSON.stringify(updatedGames));
   };
 
   return (
     <div className="games-editor p-4">
       <h2 className="text-2xl font-bold mb-4">Редактировать игры</h2>
-      <div className="games-list grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+
+      {/* Список игр с формами для редактирования */}
+      <div className="games-list grid gap-6">
         {gamesData.map((game, index) => (
           <div key={index} className="game-card bg-white p-4 rounded-lg shadow-md">
             <h3 className="text-xl font-semibold mb-2">{game.title}</h3>
+
+            {/* Поле для редактирования названия игры */}
             <div>
               <label>Название</label>
               <input
                 type="text"
-                value={newGame.title || game.title}
-                onChange={(e) => handleInputChange(e, "title")}
+                value={game.title}
+                onChange={(e) => handleInputChange(e, "title", index)}
                 className="w-full p-2 border rounded mt-2"
               />
             </div>
+
+            {/* Поле для редактирования описания игры */}
             <div>
               <label>Описание</label>
               <textarea
-                value={newGame.description || game.description}
-                onChange={(e) => handleInputChange(e, "description")}
+                value={game.description}
+                onChange={(e) => handleInputChange(e, "description", index)}
                 className="w-full p-2 border rounded mt-2"
               />
             </div>
+
+            {/* Поле для редактирования количества игроков */}
             <div>
               <label>Кол-во игроков</label>
               <input
-                type="text"
-                value={newGame.players || game.players}
-                onChange={(e) => handleInputChange(e, "players")}
+                type="number"
+                value={game.players}
+                min="2"
+                onChange={(e) => handleInputChange(e, "players", index)}
                 className="w-full p-2 border rounded mt-2"
               />
             </div>
+
+            {/* Поле для редактирования тегов */}
             <div>
               <label>Теги</label>
               <input
                 type="text"
-                value={newGame.tags || game.tags}
-                onChange={(e) => handleInputChange(e, "tags")}
+                value={game.tags}
+                onChange={(e) => handleInputChange(e, "tags", index)}
                 className="w-full p-2 border rounded mt-2"
               />
             </div>
+
+            {/* Поле для редактирования ссылки на игру */}
             <div>
               <label>Ссылка на игру</label>
               <input
                 type="text"
-                value={newGame.link || game.link}
-                onChange={(e) => handleInputChange(e, "link")}
+                value={game.link}
+                onChange={(e) => handleInputChange(e, "link", index)}
                 className="w-full p-2 border rounded mt-2"
               />
             </div>
+
+            {/* Поле для выбора изображения */}
             <div>
               <label>Изображение</label>
               <select
-                value={newGame.image || game.image}
-                onChange={handleImageChange}
+                value={game.image}
+                onChange={(e) => handleImageChange(e, index)}
                 className="w-full p-2 border rounded mt-2"
               >
                 {availableImages.map((image, idx) => (
@@ -189,17 +206,21 @@ export default function GamesEditor() {
                 ))}
               </select>
               <img
-                src={newGame.image || game.image}
+                src={game.image}
                 alt="game image"
                 className="mt-2 w-32 h-32 object-cover rounded"
               />
             </div>
+
+            {/* Кнопка сохранения изменений для игры */}
             <button
-              onClick={() => handleGameSave(index)}
+              onClick={() => handleSave(index)}
               className="bg-blue-600 text-white px-4 py-2 rounded mt-4"
             >
               Сохранить игру
             </button>
+
+            {/* Кнопка для удаления игры */}
             <button
               onClick={() => handleRemoveGame(index)}
               className="bg-red-600 text-white px-4 py-2 rounded mt-2"
@@ -208,79 +229,6 @@ export default function GamesEditor() {
             </button>
           </div>
         ))}
-      </div>
-
-      <div className="new-game-form mt-8">
-        <h3 className="text-xl font-semibold mb-4">Добавить новую игру</h3>
-        <div>
-          <label>Название</label>
-          <input
-            type="text"
-            value={newGame.title}
-            onChange={(e) => handleInputChange(e, "title")}
-            className="w-full p-2 border rounded mt-2"
-          />
-        </div>
-        <div>
-          <label>Описание</label>
-          <textarea
-            value={newGame.description}
-            onChange={(e) => handleInputChange(e, "description")}
-            className="w-full p-2 border rounded mt-2"
-          />
-        </div>
-        <div>
-          <label>Кол-во игроков</label>
-          <input
-            type="text"
-            value={newGame.players}
-            onChange={(e) => handleInputChange(e, "players")}
-            className="w-full p-2 border rounded mt-2"
-          />
-        </div>
-        <div>
-          <label>Теги</label>
-          <input
-            type="text"
-            value={newGame.tags}
-            onChange={(e) => handleInputChange(e, "tags")}
-            className="w-full p-2 border rounded mt-2"
-          />
-        </div>
-        <div>
-          <label>Ссылка на игру</label>
-          <input
-            type="text"
-            value={newGame.link}
-            onChange={(e) => handleInputChange(e, "link")}
-            className="w-full p-2 border rounded mt-2"
-          />
-        </div>
-        <div>
-          <label>Изображение</label>
-          <select
-            value={newGame.image}
-            onChange={handleImageChange}
-            className="w-full p-2 border rounded mt-2"
-          >
-            {availableImages.map((image, idx) => (
-              <option key={idx} value={image}>
-                {image.replace("/uploads/", "")}
-              </option>
-            ))}
-          </select>
-          <img
-            src={newGame.image}
-            alt="game image"
-            className="mt-2 w-32 h-32 object-cover rounded"
-          />
-        </div>
-        <button
-          onClick={handleSave}
-          className="bg-green-600 text-white px-4 py-2 rounded mt-4"
-        >
-          Добавить игру
-        </button>
       </div>
     </div>
   );
